2dcea7001175f1e060ae1c33243daac90604edf6
ilia <ilia100@hotmail.com>
2023-01-25 18:40:51 +0300

    Сделана 21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -4,4 +4,16 @@ A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53*/
 
 
+Console.WriteLine("Введите координаты x,y,z первой точки");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты x,y,z второй  точки");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
 
+
+
+double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+System.Console.WriteLine($"расстояние в 3D пространстве между точками {result:f2}");
